4d774a784749547fcf52e525d4c6f8676b5c6353
ldci4 <anoletenu@gmail.com>
2024-10-16 18:02:15 +0700

    actually

--- eazfuscator-trial-remove/Logger.cs
+++ eazfuscator-trial-remove/Logger.cs
@@ -11,11 +11,6 @@ namespace eaztrialremove
             Console.ResetColor();
         }
         public static void Log(string text, ConsoleColor color = ConsoleColor.White) => WriteLine("[L]", text, color);
-        public static void LogVerbose(string text, ConsoleColor color)
-        {
-            if (!Config.v) return;
-
-            WriteLine("[V]", text, color);
-        }
+        public static void LogVerbose(string text, ConsoleColor color) { if (Config.v) WriteLine("[V]", text, color); }
     }
 }
